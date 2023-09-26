37bd06180ebf0d4d97440efc5e166d60d394642f
tonni-akter-dev <tanniakhter123@gmail.com>
2023-09-26 13:01:27 +0600

    veichle info design done

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import TabsSection from "../Tabs/TabsSection";
+import VeichlesInfo from "../VeichlesInfo/VeichlesInfo";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <TabsSection />
+      <VeichlesInfo/>
     </div>
   );
 };
---
+++ src/Components/VeichlesInfo/VeichlesInfo.tsx
@@ -0,0 +1,43 @@
+import { Row } from "react-bootstrap";
+import "./VeichlesInfo.css";
+import { Col } from "react-bootstrap";
+import t1 from "../../assets/1.png";
+import t2 from "../../assets/t21.png";
+import t3 from "../../assets/t31.png";
+const VeichlesInfo = () => {
+  return (
+    <div className="container_width ">
+      <Row className="veichleInfo_margin">
+        <Col md={4} sm={12}>
+          <div className="vinfo_div">
+            <img src={t1} alt="" />
+            <div>
+              <h5>200.000</h5>
+              <small>Informes vehiculares emitidos</small>
+            </div>
+          </div>
+        </Col>
+        <Col md={4} sm={12}>
+          <div className="vinfo_div">
+            <img src={t2} alt="" />
+            <div>
+              <h5>30.000</h5>
+              <small>Transferencias de dominio realizadas</small>
+            </div>
+          </div>
+        </Col>
+        <Col md={4} sm={12}>
+          <div className="vinfo_div">
+            <img src={t3} alt="" />
+            <div>
+              <h5>1.300</h5>
+              <small>Autos vendidos</small>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default VeichlesInfo;
